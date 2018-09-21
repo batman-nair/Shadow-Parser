2b0c31d5756f6386409b274fbb8d3a4c9343ea86
Arjun Nair <arjunnair.mec@gmail.com>
2018-09-21 19:26:03 +0530

    Made LR0 default from factory method

--- src/parser.cc
+++ src/parser.cc
@@ -6,14 +6,13 @@
 #include <string>
 
 Parser* Parser::buildParser(std::string str) {
-    if (str == "LR") {
-        std::cout<<"Making SLR ...\n";
-        return new LR0Parser();
-    }
-    else if (str == "SLR") {
+    if (str == "SLR") {
         std::cout<<"Making SLR ...\n";
         return new SLR1Parser();
     }
-    return NULL;
+    else {
+        std::cout<<"Making LR ...\n";
+        return new LR0Parser();
+    }
 }
 
